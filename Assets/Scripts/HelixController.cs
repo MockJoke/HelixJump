0c81cabefa2a91053ae7e227b37601dcba6a2c12
Parva <parvadesai404@gmail.com>
2024-05-15 23:28:50 +0530

    [Update] refactored Level Data related functionality

--- Assets/Scripts/HelixController.cs
+++ Assets/Scripts/HelixController.cs
@@ -3,16 +3,12 @@ using UnityEngine;
 
 public class HelixController : MonoBehaviour 
 {
-    private Vector3 startRotation;
-    private Vector2 lastTapPos;
-    private float helixDistance;        
-    
     [SerializeField] private Transform startPlatform;
     [SerializeField] private Transform endPlatform;
-    [SerializeField] private GameObject helixLevelPrefab;
+    [SerializeField] private GameObject helixRingPrefab;
 
-    public List<Stage> allStages = new List<Stage>();       //all stages    
-    private List<GameObject> spawnedLevels = new List<GameObject>();        //all levels, every single level is a GameObject itself
+    [SerializeField] private LevelData levelData; 
+    private readonly List<GameObject> spawnedRings = new List<GameObject>();
 
     [Header("Components")] 
     [SerializeField] private Renderer helixRenderer;
@@ -20,7 +16,11 @@ public class HelixController : MonoBehaviour
 
     [Space] 
     [SerializeField] private Renderer ballRenderer;
-
+    
+    private Vector3 initRotation;
+    private float pillarHeight;
+    private Vector2 lastTapPos;
+    
     void Awake() 
     {
         if (mainCamera == null)
@@ -32,8 +32,9 @@ public class HelixController : MonoBehaviour
         if (ballRenderer == null)
             ballRenderer = FindObjectOfType<BallController>().GetComponent<Renderer>();
             
-        startRotation = transform.localEulerAngles;
-        helixDistance = startPlatform.localPosition.y - (endPlatform.localPosition.y + .1f);         //distance bw top and goal platform     
+        initRotation = transform.localEulerAngles;
+        pillarHeight = startPlatform.localPosition.y - (endPlatform.localPosition.y + .1f);         //distance bw top and goal platform
+        
         LoadStage(0);
     }
 	
@@ -59,57 +60,45 @@ public class HelixController : MonoBehaviour
         }
     }
 
-    public void LoadStage(int stageNumber)
+    public void LoadStage(int levelNumber)
     {
-        //get the correct stage
-        Stage stage = allStages[Mathf.Clamp(stageNumber, 0, allStages.Count - 1)];      
+        levelNumber = Mathf.Clamp(levelNumber, 0, levelData.levels.Count - 1);
+        
+        Level level = levelData.levels[levelNumber];      
 
-        if(stage == null)
-        {
-            Debug.LogError("No stage " + stageNumber + " found in allStages list (HelixController). All stages assigned in list?");
-            return;
-        }
-
-        //set the new background color in new stage
-        mainCamera.backgroundColor = allStages[stageNumber].stageBackgroundColor;
-
-        //set the ball color in new stage
-        ballRenderer.material.color = allStages[stageNumber].stageBallColor;
-
-        //set the helix color in new stage
-        helixRenderer.material.color = allStages[stageNumber].HelixColor;
-
-        //reset the helix rotation
-        transform.localEulerAngles = startRotation;
+        mainCamera.backgroundColor = levelData.levels[levelNumber].BgColor;
+        ballRenderer.material.color = levelData.levels[levelNumber].BallColor;
+        helixRenderer.material.color = levelData.levels[levelNumber].PillarColor;
+        transform.localEulerAngles = initRotation;
 
         //destroy the old levels if there are some
-        foreach(GameObject GameObj in spawnedLevels)
+        foreach(GameObject GameObj in spawnedRings)
         {
             Destroy(GameObj);
         }
 
         //create the new levels
-        float levelDistance = helixDistance / stage.levels.Count;       //how far apart each platform should be
-        float spawnPosY = startPlatform.localPosition.y;                //for pos of level, initially set to top platform and then will substract from it
+        float levelDistance = pillarHeight / level.rings.Count;       //how far apart each platform should be
+        float spawnPosY = startPlatform.localPosition.y;                //for pos of level, initially set to top platform and then will subtract from it
 
-        for(int i = 0; i < stage.levels.Count; i++)
+        for(int i = 0; i < level.rings.Count; i++)
         {
             spawnPosY -= levelDistance;         //subtracting distance bw two levels to get the pos for next level
 
-            GameObject level = Instantiate(helixLevelPrefab, transform);        //instantiating a level 
+            GameObject ring = Instantiate(helixRingPrefab, transform);        //instantiating a level 
             Debug.Log("Levels Spawned");
-            level.transform.localPosition = new Vector3(0, spawnPosY, 0);
-            spawnedLevels.Add(level);           //adding level to the list
+            ring.transform.localPosition = new Vector3(0, spawnPosY, 0);
+            spawnedRings.Add(ring);           //adding level to the list
 
             //disable some parts (depending on level setup)
-            int partsToDisable = 12 - stage.levels[i].partCount;            //12 -> one stage consists of 12 parts 
+            int partsToDisable = 12 - level.rings[i].totalSections;            //12 -> one stage consists of 12 parts 
             List<GameObject> disabledParts = new List<GameObject>();        //list of disabled parts
 
             Debug.Log("Should disable " + partsToDisable);
 
             while(disabledParts.Count < partsToDisable)            
             {
-                GameObject randomPart = level.transform.GetChild(Random.Range(0, level.transform.childCount)).gameObject;           //to randomise the disabled parts
+                GameObject randomPart = ring.transform.GetChild(Random.Range(0, ring.transform.childCount)).gameObject;           //to randomise the disabled parts
                 
                 if (!disabledParts.Contains(randomPart))
                 {
@@ -122,9 +111,9 @@ public class HelixController : MonoBehaviour
             //mark parts as death parts
             List<GameObject> leftParts = new List<GameObject>();            //list of death parts
 
-            foreach(Transform t in level.transform)                //go through position of evry single level
+            foreach(Transform t in ring.transform)                //go through position of every single level
             {
-                t.GetComponent<Renderer>().material.color = allStages[stageNumber].stageLevelPartColor;             //set color of part
+                t.GetComponent<Renderer>().material.color = levelData.levels[levelNumber].RingColor;             //set color of part
 
                 if(t.gameObject.activeInHierarchy)         
                     leftParts.Add(t.gameObject);            //active parts are added in left parts list
@@ -135,9 +124,9 @@ public class HelixController : MonoBehaviour
             //creating death parts
             List<GameObject> deathParts = new List<GameObject>();
 
-            Debug.Log("Should mark " + stage.levels[i].deathPartCount + " death parts");
+            Debug.Log("Should mark " + level.rings[i].deathSections + " death parts");
 
-            while (deathParts.Count < stage.levels[i].deathPartCount)
+            while (deathParts.Count < level.rings[i].deathSections)
             {
                 GameObject randomPart = leftParts[(Random.Range(0, leftParts.Count))];          //to randomise the left parts
 
---
+++ Assets/Scripts/LevelData.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+[CreateAssetMenu(fileName = "LevelData", menuName = "ScriptableObjects/LevelData", order = 1)]
+public class LevelData : ScriptableObject
+{
+    public List<Level> levels = new List<Level>();
+}
+
+[Serializable]
+public class Level
+{
+    public Color BgColor = Color.white;
+    public Color RingColor = Color.white;
+    public Color PillarColor = Color.white;
+    public Color BallColor = Color.white;
+    public List<Ring> rings = new List<Ring>();
+}
+
+[Serializable]
+public struct Ring
+{
+    [Range(1,11)]
+    public int totalSections;      
+
+    [Range(0, 11)]
+    public int deathSections;
+}
