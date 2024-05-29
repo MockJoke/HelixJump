3f1ddaa399a97f2c06af708c2385a33de33502f1
Parva <parvadesai404@gmail.com>
2024-05-30 00:20:11 +0530

    [Update] added randomised color palette for levels

--- Assets/Scripts/Level/LevelData.cs
+++ Assets/Scripts/Level/LevelData.cs
@@ -6,10 +6,25 @@ using System;
 public class LevelData : ScriptableObject
 {
     public List<Level> levels = new List<Level>();
+    public List<ColorPalette> ColorPalettes = new List<ColorPalette>();
 }
 
 [Serializable]
 public class Level
+{
+    public List<RingData> rings = new List<RingData>();
+    
+    // public Color BgColor = Color.white;
+    // public Color BgVignetteColor = Color.white;
+    // public Color BallColor = Color.white;
+    // public Color PillarColor = Color.white;
+    // public Color NormalSectionColor = Color.white;
+    // public Color DangerSectionColor = Color.white;
+    // public Color GoalSectionColor = Color.white;
+}
+
+[Serializable]
+public class ColorPalette
 {
     public Color BgColor = Color.white;
     public Color BgVignetteColor = Color.white;
@@ -18,7 +33,6 @@ public class Level
     public Color NormalSectionColor = Color.white;
     public Color DangerSectionColor = Color.white;
     public Color GoalSectionColor = Color.white;
-    public List<RingData> rings = new List<RingData>();
 }
 
 [Serializable]
--- Assets/Scripts/Managers/HelixController.cs
+++ Assets/Scripts/Managers/HelixController.cs
@@ -3,12 +3,12 @@ using UnityEngine;
 using UnityEngine.Rendering;
 using UnityEngine.Rendering.Universal;
 
-public class HelixController : MonoBehaviour 
+public class HelixController : MonoBehaviour
 {
     [SerializeField] private Transform startRingTransform;
     [SerializeField] private Transform endRingTransform;
     [SerializeField] private Ring helixRingPrefab;
-    
+
     [Header("Components")] 
     [SerializeField] private Renderer helixRenderer;
 
@@ -17,23 +17,25 @@ public class HelixController : MonoBehaviour
     [SerializeField] private Volume ppVolume;
     [SerializeField] private Renderer ballRenderer;
     [SerializeField] private TrailRenderer ballTrailRenderer;
-    
-    [Space]
+
+    [Space] 
     [SerializeField] private GameObject splashPrefab;
-    
-    [Space]
-    public LevelData levelData; 
-    
+
+    [Space] 
+    public LevelData levelData;
+
     private readonly List<GameObject> spawnedRings = new List<GameObject>();
 
+    private int currLevel = 0;
     private Vignette vignette;
-    
+    private ColorPalette currColorPalette = null;
     private float pillarHeight;
+
     private Vector3 initRotation = Vector3.zero;
     private Vector3 lastTapPos = Vector3.zero;
     private bool newTap = true;
-    
-    void Awake() 
+
+    void Awake()
     {
         if (mainCamera == null)
             mainCamera = Camera.main;
@@ -48,22 +50,22 @@ public class HelixController : MonoBehaviour
             ballTrailRenderer = ballRenderer.GetComponentInChildren<TrailRenderer>();
 
         ppVolume.profile.TryGet(out vignette);
-        
+
         initRotation = transform.localEulerAngles;
-        pillarHeight = startRingTransform.localPosition.y - (endRingTransform.localPosition.y + .1f);         //distance bw top and goal platform
-        
+        pillarHeight = startRingTransform.localPosition.y - (endRingTransform.localPosition.y + .1f);
+
         // LoadLevel(0);
     }
-	
-	void Update() 
+
+    void Update()
     {
-        // spin helix by using click (or touch) & drag
+        // Spin helix by using click (or touch) & drag
         if (Input.GetMouseButton(0))
         {
             Vector3 curTapPos = Input.mousePosition;
 
             // if (lastTapPos == Vector3.zero)
-            if (newTap)    
+            if (newTap)
                 lastTapPos = curTapPos;
 
             float delta = lastTapPos.x - curTapPos.x;
@@ -72,8 +74,8 @@ public class HelixController : MonoBehaviour
             lastTapPos = curTapPos;
             newTap = false;
         }
-        
-        // set newTap once the input is released
+
+        // Set newTap once the input is released
         if (Input.GetMouseButtonUp(0))
         {
             newTap = true;
@@ -83,52 +85,75 @@ public class HelixController : MonoBehaviour
 
     public void LoadLevel(int levelNumber)
     {
-        // remove rings from prev level
+        // Remove rings from prev level
         foreach (GameObject GameObj in spawnedRings)
         {
             Destroy(GameObj);
         }
+
         spawnedRings.Clear();
-        
+
         // levelNumber = Mathf.Clamp(levelNumber, 0, levelData.levels.Count - 1);
+        
         Level level = levelData.levels[levelNumber];
-        
-        mainCamera.backgroundColor = levelData.levels[levelNumber].BgColor;
-        vignette.color.Override(levelData.levels[levelNumber].BgVignetteColor);
-        helixRenderer.material.color = levelData.levels[levelNumber].PillarColor;
-        ballRenderer.material.color = levelData.levels[levelNumber].BallColor;
-        ballTrailRenderer.materials[0].color = levelData.levels[levelNumber].BallColor;
-        splashPrefab.GetComponent<Renderer>().sharedMaterial.color = levelData.levels[levelNumber].BallColor;
+
+        if (currLevel == 0)
+        {
+            currLevel = levelNumber;
+        }
+
+        if (currColorPalette == null || currLevel != levelNumber)
+        {
+            ColorPalette colors = GetRandomColorPalette();
+            while (colors == currColorPalette)
+            {
+                colors = GetRandomColorPalette();
+            }
+
+            currColorPalette = colors;
+        }
+
+        mainCamera.backgroundColor = currColorPalette.BgColor;
+        vignette.color.Override(currColorPalette.BgVignetteColor);
+        helixRenderer.material.color = currColorPalette.PillarColor;
+        ballRenderer.material.color = currColorPalette.BallColor;
+        ballTrailRenderer.materials[0].color = currColorPalette.BallColor;
+        splashPrefab.GetComponent<Renderer>().sharedMaterial.color = currColorPalette.BallColor;
         transform.localEulerAngles = initRotation;
-        
+
         //create the new levels
-        float distBwRings = pillarHeight / (level.rings.Count + 1);           // how far apart each ring should be
-        
+        float distBwRings = pillarHeight / (level.rings.Count + 1); // how far apart each ring should be
+
         // for vertical-pos of level, initially set it to top platform and then gradually subtract from it
         float initSpawnPosY = startRingTransform.localPosition.y;
-        
+
         // Start Ring 
-        Ring startRing = Instantiate(helixRingPrefab, transform); 
+        Ring startRing = Instantiate(helixRingPrefab, transform);
         startRing.transform.localPosition = new Vector3(0, initSpawnPosY, 0);
         spawnedRings.Add(startRing.gameObject);
-        startRing.SetupAsStartRing(levelData.levels[levelNumber].NormalSectionColor);
-        
+        startRing.SetupAsStartRing(currColorPalette.NormalSectionColor);
+
         // Middle Rings
         for (int i = 0; i < level.rings.Count; i++)
         {
-            initSpawnPosY -= distBwRings;         //subtracting distance bw two rings to get the pos for next ring
+            initSpawnPosY -= distBwRings; //subtracting distance bw two rings to get the pos for next ring
 
-            Ring ring = Instantiate(helixRingPrefab, transform); 
+            Ring ring = Instantiate(helixRingPrefab, transform);
             ring.transform.localPosition = new Vector3(0, initSpawnPosY, 0);
             spawnedRings.Add(ring.gameObject);
-            
-            ring.SetupRing(level.rings[i], levelData.levels[levelNumber].NormalSectionColor, levelData.levels[levelNumber].DangerSectionColor);
+
+            ring.SetupRing(level.rings[i], currColorPalette.NormalSectionColor, currColorPalette.DangerSectionColor);
         }
-        
+
         // End Ring
-        Ring endRing = Instantiate(helixRingPrefab, transform); 
+        Ring endRing = Instantiate(helixRingPrefab, transform);
         endRing.transform.localPosition = new Vector3(0, endRingTransform.localPosition.y, 0);
         spawnedRings.Add(endRing.gameObject);
-        endRing.SetupAsEndRing(levelData.levels[levelNumber].GoalSectionColor);
+        endRing.SetupAsEndRing(currColorPalette.GoalSectionColor);
+    }
+
+    private ColorPalette GetRandomColorPalette()
+    {
+        return levelData.ColorPalettes[Random.Range(0, levelData.ColorPalettes.Count)];
     }
 }
